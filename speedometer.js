a3a1766264d35f117644c9e52165f5de2ec0eb1d
Joshua Dias <joshuadiasbf@gmail.com>
2024-03-07 20:52:10 -0300

    Armazenando os dados no localStorage

--- speedometer.js
+++ speedometer.js
@@ -3,12 +3,14 @@ const startBtn = document.querySelector("#start")
 const stopBtn = document.querySelector("#stop")
 
 let watchID = null;
+let currentRide = null;
 startBtn.addEventListener("click", () => {
 
     if (watchID)
         return
 
     function handleSuccess(position) {
+        addPosition(currentRide, position)
         console.log(position)
         speedElement.innerText = position.coords.speed ?
             (position.coords.speed * 3.6).toFixed(1) : 0
@@ -18,8 +20,8 @@ startBtn.addEventListener("click", () => {
     }
 
     const options = { enableHighAccuracy: true }
+    currentRide = createNewRide()
     watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, options)
-
     startBtn.classList.add("d-none")
     stopBtn.classList.remove("d-none")
 
@@ -32,6 +34,8 @@ stopBtn.addEventListener("click", () => {
 
     navigator.geolocation.clearWatch(watchID)
     watchID = null
+    updatedStopTime(currentRide)
+    currentRide = null
     startBtn.classList.remove("d-none")
     stopBtn.classList.add("d-none")
 
